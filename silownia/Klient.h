6216d98b3ca3d979dbf0e764bd08f8312aacb1fe
nataviaa <65811938+nataviaa@users.noreply.github.com>
2021-01-08 16:14:40 +0100

    Update Klient.h

--- silownia/Klient.h
+++ silownia/Klient.h
@@ -6,35 +6,38 @@ using namespace std;
 #ifndef __Klient_h__
 #define __Klient_h__
 
-// #include "Karta_zdrowia.h"
+#include "Karta_zdrowia.h"
 #include "Wizyta_u_dietetyla.h"
 #include "Trening_personalny.h"
-#include "Grupa_zajêciowa.h"
+#include "Grupa_zajÃªciowa.h"
 
 class Karta_zdrowia;
 class Wizyta_u_dietetyla;
 class Trening_personalny;
-class Grupa_zajêciowa;
+class Grupa_zajeciowa;
 class Klient;
 
 class Klient
 {
-	private: String _imiê;
-	private: String _nazwisko;
-	private: int _rok_urodzenia;
-	private: string _imie_rodzica;
-	private: string _nazwiski_rodzica;
-	private: char _numer_telefonu_opiekuna_prawnego;
-	public: Karta_zdrowia* _unnamed_Karta_zdrowia_;
-	public: std::vector<Wizyta_u_dietetyla*> _unnamed_Wizyta_u_dietetyla_;
-	public: std::vector<Trening_personalny*> _unnamed_Trening_personalny_;
-	public: std::vector<Grupa_zajêciowa*> _unnamed_Grupa_zajêciowa_;
+private:
+	String _imie;
+	String _nazwisko;
+	int _rok_urodzenia;
+	string _imie_rodzica;
+	string _nazwiski_rodzica;
+	char _numer_telefonu_opiekuna_prawnego;
+	Karta_zdrowia* _unnamed_Karta_zdrowia_;
+	Wizyta_u_dietetyla* _unnamed_Wizyta_u_dietetyla_;
+	Trening_personalny* _unnamed_Trening_personalny_;
+	Grupa_zajeciowa*; _unnamed_Grupa_zajeciowa;
 
-	public: void zapisz_na_zajecia();
+public: void zapisz_do_grupy_zajeciowej();
+	void zapisz_do_dietetyka();
+	void zapisz_na_trening();
 
-	public: Klient uaktualnij_dane(string aImie, string aNazwisko, string aImie_rodzica, string aNazwisko_rodzica, char aNumer_telefonu_rodzica);
-
-	public: void wglad_w_karte_zdrowia();
+	Klient uaktualnij_dane(string aImie, string aNazwisko, string aImie_rodzica, string aNazwisko_rodzica, char aNumer_telefonu_rodzica);
+	void dodaj_karte_zdrowia();
+	void wglad_w_karte_zdrowia();
 };
 
 #endif
