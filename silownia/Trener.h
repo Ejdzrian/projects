12647d5a826a4c84d4fa151b61f19997e8eac715
nataviaa <65811938+nataviaa@users.noreply.github.com>
2021-01-08 16:40:02 +0100

    Update Trener.h

--- silownia/Trener.h
+++ silownia/Trener.h
@@ -7,33 +7,27 @@ using namespace std;
 #define __Trener_h__
 
 #include "Karta_zdrowia.h"
-#include "Grupa_zajêciowa.h"
-// #include "Wizyta_u_dietetyla.h"
+#include "Grupa_zajÃªciowa.h"
 #include "Trening_personalny.h"
 #include "Pracownik.h"
 
 class Karta_zdrowia;
-class Grupa_zajêciowa;
-class Wizyta_u_dietetyla;
+class Grupa_zajeciowa;
+class Wizyta_u_dietetyka;
 class Trening_personalny;
-// class Pracownik;
+class Pracownik;
 class Trener;
 
 class Trener: public Pracownik
 {
-	private: string _specjalizacja;
+	
 	public: std::vector<Karta_zdrowia*> _unnamed_Karta_zdrowia_;
-	public: Trener* _unnamed_Trener_;
-	public: Trener* _unnamed_Trener_2;
-	public: std::vector<Grupa_zajêciowa*> _unnamed_Grupa_zajêciowa_;
-	public: Wizyta_u_dietetyla* _unnamed_Wizyta_u_dietetyla_;
+	
+	public: std::vector<Grupa_zajÃªciowa*> _unnamed_Grupa_zajÃªciowa_;
+	
 	public: std::vector<Trening_personalny*> _unnamed_Trening_personalny_;
 
-	public: void stwórz_plan_treningowy();
-
-	public: void modyfikuj_plan_treningowy(string aUnnamed__8);
-
-	public: string dostêpnoœæ_godzinowa();
+	public: int[70] dostepnosc_godzinowa();//
 };
 
 #endif
